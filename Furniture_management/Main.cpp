cdf68b86057b9a9a2d8caf8655c2038096b41952
Dragoonjack <69187451+Dragoonjack@users.noreply.github.com>
2025-06-22 23:35:00 +0800

    Update Main.cpp

--- Furniture_management/Main.cpp
+++ Furniture_management/Main.cpp
@@ -5,14 +5,5 @@ using namespace std;
 
 int main() {
 	InventorySystem system;
-	//initialize product info for testing stage
-	Product* p1 = new Product("P456", "chair", "black", 199.99, 10);
-	Product* p2 = new Product("P4199", "bed", "white", 1.33, 90);
-
-	system.dept[BEDROOM]->addItem(p1);
-	system.dept[BEDROOM]->addItem(p2);
-
-	system.dept[BEDROOM]->displayItem();
-
-	return 0;
+	system.menu();
 }
